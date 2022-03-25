b55f13247c2a4362fb6a37a994392250420060fe
woals7480 <94383932+woals7480@users.noreply.github.com>
2022-03-25 21:48:14 +0900

    update movie-practice

---
+++ src/routes/Detail.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState();
+  const { movieId } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/movie_details.json?movie_id=${movieId}`
+      )
+    ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+    console.log(json.data.movie);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Detail Loading</h1>
+      ) : (
+        <div>
+          <img src={movie.background_image} alt="" />
+          <h1>{movie.title_long}</h1>
+          <p>{movie.description_full}</p>
+          <ul>
+            {movie.genres.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Detail;
